50fafc9a47adfcf3c29769ae7c0e8d72eb64012a
vova <vovaprog@yandex.ru>
2016-01-21 18:59:14 +0400

    add 616b

---
+++ sorter_c/616b.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int nofRows, nofCols;
+    cin >> nofRows;
+    cin >> nofCols;
+    
+    int result=-1;
+    
+    for(int i=0;i<nofRows;i++)
+    {
+        int rowMin=-1;
+        for(int j=0;j<nofCols;j++)
+        {
+            int v;
+            cin >> v;
+            if(j==0 || v<rowMin) rowMin=v;
+        }
+        
+        if(i==0 || rowMin>result)
+        {
+            result = rowMin;
+        }
+    }
+    
+    cout << result;
+        
+    return 0;
+}
