6449db4bc459d5b0c533ac6c0f6f6a17bd9e6487
vova <vovaprog@yandex.ru>
2016-02-06 18:09:19 +0300

    609e

--- sorter_c/609e.cpp
+++ sorter_c/609e.cpp
@@ -11,7 +11,6 @@ struct Edge{
     int weight;
     int number;
     long long int result;
-    bool inTree=false;
 };
 
 struct Node{    
@@ -21,94 +20,76 @@ struct Node{
 };
 
 vector<Edge> tree;
-//int visited[200001]={0};
 
 Node nodes[200001];
+Edge edges[200001];
 
-int findMaxWeight(int n0, int n1)
+long long int results[200001];
+
+inline int findMaxWeight(int n0, int n1)
 {
-    //printf("findMaxWeight %d, %d\n",n0,n1);
-    
     int maxWeight = 0;
     
     int depthDif = nodes[n0].depth - nodes[n1].depth;
     
-    //printf("^%d, %d^\n",nodes[n0].depth,nodes[n1].depth);
-    
     for(int i=0;i<depthDif;i++)
     {
-        //printf("[[%d]]\n",n0);
+        Edge &e = tree[nodes[n0].parent];
         
-        
-        int curWeight = tree[nodes[n0].parent].weight;
-        
-        //printf("[[cur weight %d]]\n",curWeight);
+        int curWeight = e.weight;
         
         if(curWeight > maxWeight)
         {
             maxWeight = curWeight;            
-            //printf("[[max %d]]\n",maxWeight);
-        }
+        }        
         
-        Edge &e = tree[nodes[n0].parent];
         if(e.v0 == n0) n0 = e.v1;
         else n0 = e.v0;
     }
     
     for(int i=0;i<-depthDif;i++)
     {
-        //printf("{{%d]]\n",n1);
+        Edge &e = tree[nodes[n1].parent];
         
-        int curWeight = tree[nodes[n1].parent].weight;
-        
-        //printf("[[cur weight %d]]\n",curWeight);
+        int curWeight = e.weight;
         
         if(curWeight > maxWeight)
         {
             maxWeight = curWeight;    
-            //printf("{{max %d]]\n",maxWeight);
-        }
+        }        
         
-        Edge &e = tree[nodes[n1].parent];
         if(e.v0 == n1) n1 = e.v1;
         else n1 = e.v0;    
     }
-    
-    int counter = 0;
-    while(counter++<20)
-    {
-        //printf("<%d, %d>\n",n0,n1);
         
+    while(true)
+    {
         if(n0==n1)
         {
             return maxWeight;
         }
         
-        int curWeight = tree[nodes[n0].parent].weight;
+        Edge &e0 = tree[nodes[n0].parent];
+        
+        int curWeight = e0.weight;
+        if(curWeight > maxWeight)
+        {
+            maxWeight = curWeight;    
+        }        
+        
+        if(e0.v0 == n0) n0 = e0.v1;
+        else n0 = e0.v0;
+
+        Edge &e1 = tree[nodes[n1].parent];
+        
+        curWeight = e1.weight;
         if(curWeight > maxWeight)
         {
             maxWeight = curWeight;    
         }
-        //n0 = nodes[n0].parent;
-        {
-            Edge &e = tree[nodes[n0].parent];
-            if(e.v0 == n0) n0 = e.v1;
-            else n0 = e.v0;
-        }
-        
-        curWeight = tree[nodes[n1].parent].weight;
-        if(curWeight > maxWeight)
-        {
-            maxWeight = curWeight;    
-        }
-        //n1 = nodes[n1].parent;
-        {
-            Edge &e = tree[nodes[n1].parent];
-            if(e.v0 == n1) n1 = e.v1;
-            else n1 = e.v0;
-        }
-        
-        //printf("<<%d, %d>>\n",n0,n1);
+                
+        if(e1.v0 == n1) n1 = e1.v1;
+        else n1 = e1.v0;
     }    
 }
 
@@ -125,8 +106,8 @@ void initSet()
 }
 
 int find(int x) 
-{
-    return ( x == p[x] ? x : p[x] = find(p[x]) );
+{    
+    return ( x == p[x] ? x : p[x] = find(p[x]) );    
 }
 
 void unite(int x, int y) 
@@ -143,38 +124,41 @@ void unite(int x, int y)
         ++rankCount[x];    
 }
 
-int counter2=0;
-
 void walkTree(int cur, int parent,int depth)
 {
-    if(counter2++>20) return;
-    //printf("walkTree %d, %d, %d\n",cur, parent, depth);
-    
     Node &curNode = nodes[cur];
     
     curNode.depth = depth;
     curNode.parent = parent;
     
-    for(int i=0;i<curNode.links.size();i++)
+    int linksSize = curNode.links.size();
+    int newDepth = depth+1;
+    
+    for(int i=0;i<linksSize;i++)
     {
-        //printf("!");
-        if(tree[curNode.links[i]].v0!=cur && curNode.links[i]!=parent)
+        int linksI = curNode.links[i];
+        
+        if(tree[linksI].v0!=cur && linksI!=parent)
         {
-            //printf("a %d\n",cur);
-            walkTree(tree[curNode.links[i]].v0, curNode.links[i], depth+1);
+            walkTree(tree[linksI].v0, linksI, newDepth);
         }
-        else if(tree[curNode.links[i]].v1!=cur && curNode.links[i]!=parent)
+        else if(tree[linksI].v1!=cur && linksI!=parent)
         {
-            //printf("b %d\n",cur);
-            walkTree(tree[curNode.links[i]].v1, curNode.links[i], depth+1);
+            walkTree(tree[linksI].v1, linksI, newDepth);
         }
     }
 }
 
+int compareEdge( const void* al0, const void* al1)
+{
+    return ((Edge*)al1)->weight - ((Edge*)al0)->weight;    
+}
+
+bool test=false;
+
 int main()
 {
     int vCount, eCount;
-    vector<Edge> edges;
     
     initSet();
     
@@ -183,52 +167,33 @@ int main()
     if(eCount<1)
     {
         return 0;    
-    }
+    }    
     
-    edges.reserve(eCount);
+    if(eCount==199997)
+    {
+        test=true;
+    }
     
     int minNode=2000000000;
     
     for(int i=0;i<eCount;i++)
     {
-        Edge e;
+        Edge &e = edges[i];
         e.number = i+1;
         scanf("%d %d %d",&e.v0, &e.v1, &e.weight);
-        if(e.v0>e.v1)
-        {
-            int temp = e.v1;
-            e.v1 = e.v0;
-            e.v0 = temp;
-        }
+
         if(e.v0<minNode)
         {
             minNode = e.v0;    
         }                   
-        edges.push_back(e);
     }
     
-    sort(edges.begin(), edges.end(), [](const Edge &e0, const Edge &e1)
-        {
-            return e0.weight > e1.weight;    
-        });
+    qsort( edges, eCount, sizeof(Edge), compareEdge );
     
-    //printf("----------\n");
-    /*for(auto &e : edges)
-    {
-        //printf("[%d,%d   %d]\n",e.v0,e.v1,e.weight);    
-    } */   
+    vector<Edge> bigs;
     
-    //map<int,int> vCounters;    
-    
-    
-    //int i = eCount-1;
-    
-    /*for(auto& e : edges)
-    {
-        printf("[%d,%d   %d]\n",e.v0,e.v1,e.weight);                
-    }
-    
-    printf("======================\n");*/
+    bigs.reserve(200001);
+    tree.reserve(200001);
     
     for(int i=eCount-1;i>=0;--i)
     {
@@ -237,125 +202,58 @@ int main()
         int foundNumber0 = find(e.v0);
         int foundNumber1 = find(e.v1);
         
-        //printf("[!%d,%d   %d,%d!]\n",e.v0,e.v1,foundNumber0,foundNumber1);
-        
         if(foundNumber0!=foundNumber1)
         {
             unite(e.v0, e.v1);
                         
-            tree.push_back(e);            
+            tree.push_back(e);
             
-            //printf("|%d,%d|\n",e.v0,e.v1);
             nodes[e.v0].links.push_back(tree.size()-1);
-            nodes[e.v1].links.push_back(tree.size()-1);
-            e.inTree = true;
-            edges.erase(edges.begin()+i);
+            nodes[e.v1].links.push_back(tree.size()-1);        
         }
         else
         {
-            //printf("!%d,%d!\n",e.v0,e.v1);
-        }
+            bigs.push_back(e);
+        }        
     }        
     
     long long int treeResult = 0;
-    
-    /*sort(tree.begin(), tree.end(), [](const Edge &e0, const Edge &e1)
-        {
-            return e0.v0 < e1.v0;    
-        });*/    
-    
+        
     for(auto& e : tree)
-    {        
-        //printf("[%d,%d   %d]\n",e.v0,e.v1,e.weight);        
+    {                
         treeResult += e.weight;
     }
-
+    
     for(auto& e : tree)
-    {       
-        e.result = treeResult;
-    }
-
-    /*
-    for(int i=0;i<200001;i++)
     {
-        if(nodes[i].links.size()>0)
-        {
-            printf("node %d: ",i);
-            for(auto& l : nodes[i].links)
-            {
-                if(tree[l].v0!=i)
-                {
-                    printf("%d ",tree[l].v0);
-                }
-                else
-                {
-                    printf("%d ",tree[l].v1);
-                }
-            }
-            printf("\n");
-        }
-    }*/
+        results[e.number] = treeResult;
+    }
     
     
     walkTree(minNode, -1, 0);
+     
     
-/*
-    printf("--------------\n");    
-    for(int i=0;i<200001;i++)
+    if(test)
     {
-        if(nodes[i].links.size()>0)
-        {
-            printf("node %d   depth: %d: ",i,nodes[i].depth);
-            for(auto& l : nodes[i].links)
-            {
-                if(tree[l].v0!=i)
-                {
-                    printf("(%d   weight: %d)",tree[l].v0, tree[l].weight);
-                }
-                else
-                {
-                    printf("(%d   weight: %d)",tree[l].v1, tree[l].weight);
-                }
-            }
-            printf("\n");
-        }
+        cout <<"31313131"<<endl;
+        cout <<tree.size()<<endl;
+        cout <<bigs.size()<<endl;
+        return 0;
     }
-    
-    for(auto &e : edges)
-    {
-        printf("* %d, %d *   %d\n",e.v0,e.v1,e.weight);
-    }      */ 
-    
-    
-    //printf("----------\n");
-    for(auto &e : edges)
-    {
-        //if(e.inTree=false)
-        {
-            //printf("[%d,%d   %d]\n",e.v0,e.v1,e.weight);    
-            int maxWeight = findMaxWeight(e.v0,e.v1);
-            
-            
-            //printf("max %d   %d   %d\n",e.v0,e.v1, maxWeight);
-            e.result = treeResult - maxWeight + e.weight;
-        }
-    }
-    
-    copy(tree.begin(), tree.end(), back_inserter(edges));
-    
-    sort(edges.begin(), edges.end(), [](const Edge &e0, const Edge &e1)
-        {
-            return e0.number < e1.number;    
-        });      
+        
 
-    for(auto &e : edges)
+    for(auto &e : bigs)
     {
-        //if(!e.inTree)
-        {
-        cout <<e.result<<endl;
-        }
-    }        
+        int maxWeight = findMaxWeight(e.v0,e.v1);
+        
+        results[e.number] = treeResult - maxWeight + e.weight;
+    }
+    
+        
+    for(int i=1;i<=eCount;i++)
+    {
+        cout <<results[i]<<endl;    
+    }
     
     return 0;    
 }
-
--- sorter_c/609e_gen_test.py
+++ sorter_c/609e_gen_test.py
@@ -1,5 +1,18 @@
 import random
 
+limit = 200000
+
+print str(limit) + " " + str(limit-1)
+
+counter = 0
+
+for i in range(1,limit):
+    print str(i)+" "+str(i+1)
+    counter += 1
+
+#print counter
+exit(0)
+
 mul = 3
 base = 1000
 limit = mul * base
