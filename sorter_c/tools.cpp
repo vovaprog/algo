b572c51b5576998f02ed3537a8ca069c98d4d39c
vova <vovaprog@yandex.ru>
2016-02-20 18:45:08 +0400

    add tools.cpp

---
+++ sorter_c/tools.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void debug(const char *format, ...)
+{
+    /*va_list args;
+    va_start(args, format);
+    vprintf(format, args);
+    va_end(args);*/            
+}
+
+
+nextSet->insert(r);
+nextSet->erase(r);  
+nextSet->count(r);
+
+
+template<class TMap,class TKey,class TValue>
+inline bool tryGetValue(TMap &m, TKey &key, TValue &u)
+{
+    auto it = m.find(key);
+    if(it!=m.end())
+    {
+        u = it->second;
+        return true;
+    }
+    return false;
+}
+
+
