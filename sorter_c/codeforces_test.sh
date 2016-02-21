304c0870b0806d02a2c2624c8f560afcb607725e
vova <vova@localhost.localdomain>
2016-02-21 10:06:05 +0300

    pch

--- sorter_c/codeforces_test.sh
+++ sorter_c/codeforces_test.sh
@@ -4,7 +4,7 @@ PROGRAM=613d_3
 
 printf "\n\n\n\n\n\n\n\n"
 
-g++ -std=c++11 -I. -include stdc++.h -H -g -O2 ./$PROGRAM.cpp -o ./$PROGRAM || exit 1
+g++ -std=c++11 -I. -include stdc++.h -g -O2 ./$PROGRAM.cpp -o ./$PROGRAM || exit 1
 #g++ -std=c++11 -g -O2 ./$PROGRAM.cpp -o ./$PROGRAM || exit 1
 
 printf "\n\nbuild time: $SECONDS\n\n"
