54555c7edebc55f199a269f8ca4ef587aa0fa791
vova <vova@localhost.localdomain>
2016-02-21 09:22:09 +0300

    use pch

--- sorter_c/codeforces_test.sh
+++ sorter_c/codeforces_test.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-PROGRAM=343a
+PROGRAM=613d_3
 
 printf "\n\n\n\n\n\n\n\n"
 
-g++ -std=c++11 -I. -include stdc++.h  -H -g -O2 ./$PROGRAM.cpp -o ./$PROGRAM || exit 1
+g++ -std=c++11 -I. -include stdc++.h -H -g -O2 ./$PROGRAM.cpp -o ./$PROGRAM || exit 1
+#g++ -std=c++11 -g -O2 ./$PROGRAM.cpp -o ./$PROGRAM || exit 1
+
+printf "\n\nbuild time: $SECONDS\n\n"
 
 bash ./codeforces_run_test.sh $PROGRAM
 
--- sorter_c/tools.cpp
+++ sorter_c/tools.cpp
@@ -33,3 +33,20 @@ usersEmplace.emplace(key, "abc");
 usersIndex[key] = User("abc");
 usersInsert.insert(pair<string,User>(key, User("abc")));
 
+
+/*
+Про stdc++.h
+
+В программе пишешь один #include <bits/stdc++.h>
+Вызываешь (надо папку с stdc++ найти):
+g++ -Winvalid-pch -x c++-header -g -O2 -o ./stdc++.h.gch /usr/include/i386-linux-gnu/c++/4.9/bits/stdc++.h
+
+Потом билдишь:
+g++ -I. -include stdc++.h -H -g -O2 ./$PROGRAM.cpp -o ./$PROGRAM
+
+Начинает все супер быстро билдиться!
+
+Описание:
+https://gcc.gnu.org/onlinedocs/libstdc++/manual/using_headers.html
+*/	
+	
