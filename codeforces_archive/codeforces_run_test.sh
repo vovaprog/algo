370588127edc3c322b187f380090aee86275c780
vova <vovaprog@yandex.ru>
2016-03-22 07:51:03 +0300

    gitignore

--- codeforces_archive/codeforces_run_test.sh
+++ codeforces_archive/codeforces_run_test.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 PROGRAM=$1
+PROGRAM_FOLDER=$2
+
+if [ -z "$PROGRAM_FOLDER" ]
+    then PROGRAM_FOLDER="./"    
+fi
 
 for i in ./test_data/$PROGRAM/*.txt
 do
     echo "====== $i ======"
     SECONDS=0
-    cat $i | ./$PROGRAM
+    cat $i | $PROGRAM_FOLDER/$PROGRAM
     RUN_SECONDS=$SECONDS
     
     if [ -f $i.answer ] 
