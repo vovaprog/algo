45a62685ed80e8a06688a5eb288a129d2e126c64
vova <vova@localhost.localdomain>
2016-05-14 22:06:58 +0300

    debug function

--- codeforces_archive/codeforces_template_streams.cpp
+++ codeforces_archive/codeforces_template_streams.cpp
@@ -4,7 +4,16 @@ using namespace std;
 
 #define debug(args...) printf(args);
 #define debug2(fmt,args...) printf(fmt,args);printf("   (%s)\n",#args);
-#define debug3(arg) cout << #arg << " = " << arg << "\n";
+
+//=====================================================================
+#define debug3(args...) { vector<string> v = splitArgNames(#args); debugPrint(v.begin(), args); }
+vector<string> splitArgNames(const string& s) 
+{ vector<string> v; stringstream ss(s); for (string x; getline(ss, x, ',');) { v.emplace_back(x); } return move(v); }
+void debugPrint(vector<string>::iterator it) { cout <<"\n"; }
+template<typename T, typename... Args>
+void debugPrint(vector<string>::iterator it, T head, Args... args) 
+{ cout << *it <<" = "<< head << "     "; debugPrint(++it, args...); }
+//=====================================================================
 
 typedef long long int int64;
 
@@ -23,6 +32,6 @@ int main()
         cin >> r;
     }
 
-
+    
     return 0;
 }
