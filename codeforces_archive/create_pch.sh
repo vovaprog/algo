104198d277d46e4cbba553a1ce17f665c62142ea
gituser <gituser@static.166.191.47.78.clients.your-server.de>
2016-06-05 22:57:14 +0200

    create_pch

--- codeforces_archive/create_pch.sh
+++ codeforces_archive/create_pch.sh
@@ -1,4 +1,5 @@
 #g++ -std=c++11 -Winvalid-pch -x c++-header -g -O2 -o ./stdc++.h.gch /usr/include/i386-linux-gnu/c++/4.9/bits/stdc++.h
+#g++ -std=c++11 -Winvalid-pch -x c++-header -g -O2 -o ./stdc++.h.gch /usr/include/x86_64-linux-gnu/c++/4.9/bits/stdc++.h
 g++ -std=c++11 -Winvalid-pch -x c++-header -g -O2 -o ./stdc++.h.gch /usr/include/x86_64-linux-gnu/c++/4.8/bits/stdc++.h
 #g++ -std=c++11 -Winvalid-pch -x c++-header -g -O2 -o ./stdc++.h.gch /usr/include/x86_64-linux-gnu/c++/5/bits/stdc++.h
 
