6b15e9a468fc321031bd538e719de859c77b0e1e
Vladimir Silaev <vovaprog@yandex.ru>
2016-11-25 23:43:35 +0300

    740b

--- codeforces_archive/740b.cpp
+++ codeforces_archive/740b.cpp
@@ -2,22 +2,8 @@
 
 using namespace std;
 
-#define debug(args...) printf(args);
-#define debug2(fmt,args...) printf(fmt,args);printf("   (%s)\n",#args);
-
-//=====================================================================
-#define debug3(args...) { vector<string> v = splitArgNames(#args); debugPrint(v.begin(), args); }
-vector<string> splitArgNames(const string& s) 
-{ vector<string> v; stringstream ss(s); for (string x; getline(ss, x, ',');) { v.emplace_back(x); } return move(v); }
-void debugPrint(vector<string>::iterator it) { cout <<"\n"; }
-template<typename T, typename... Args>
-void debugPrint(vector<string>::iterator it, T head, Args... args) 
-{ cout << *it <<" = "<< head << "     "; debugPrint(++it, args...); }
-//=====================================================================
-
-typedef long long int int64;
-
-struct Sub{
+struct Sub
+{
     int start, end, index, value = 0;
 };
 
@@ -37,11 +23,10 @@ int main()
 {
     std::ios::sync_with_stdio(false);
     cin.tie(0);
-   
+
     int nFlowers, nSubs;
 
-    istringstream ss("5 4\n1 -2 1 3 -4\n1 2\n4 5\n3 4\n1 4\n");
-    ss >> nFlowers >> nSubs;
+    cin >> nFlowers >> nSubs;
 
     vector<int> flowers;
     flowers.reserve(nFlowers);
@@ -49,37 +34,37 @@ int main()
     for(int i = 0; i < nFlowers; ++i)
     {
         int f;
-        ss >> f;
+        cin >> f;
         flowers.push_back(f);
     }
 
-    Sub subs[110];
+    Sub subs[100];
     vector<Sub*> subStarts, subEnds;
     subStarts.reserve(nSubs);
     subEnds.reserve(nSubs);
 
-    for(int i=0; i< nSubs; ++i)
+    for(int i = 0; i < nSubs; ++i)
     {
-        ss >> subs[i].start >> subs[i].end;
+        cin >> subs[i].start >> subs[i].end;
         subs[i].index = i + 1;
         subStarts.push_back(&subs[i]);
         subEnds.push_back(&subs[i]);
     }
-    
+
     sort(subStarts.begin(), subStarts.end(), compSubStarts);
-    sort(subEnds.begin(), subEnds.end(), compSubEnds); 
+    sort(subEnds.begin(), subEnds.end(), compSubEnds);
 
     set<int> curSubs;
     int curSubStarts = 0, curSubEnds = 0;
 
-    for(int i=1;i <= nFlowers;++i)
+    for(int i = 1; i <= nFlowers; ++i)
     {
-        while(curSubStarts < nSubsi>=subStarts[curSubStarts]->start && )
+        while(curSubStarts < nSubs && i >= subStarts[curSubStarts]->start)
         {
             curSubs.insert(subStarts[curSubStarts]->index);
             ++curSubStarts;
         }
-        while(i>subEnds[curSubEnds]->end && curSubEnds < nSubs)
+        while(curSubEnds < nSubs && i > subEnds[curSubEnds]->end)
         {
             curSubs.erase(subEnds[curSubEnds]->index);
             ++curSubEnds;
